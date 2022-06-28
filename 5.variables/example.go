cf7286abd50c190149b4c6fe1c25e95e68259082
hungvx <vuxuanhungmta@gmail.com>
2022-06-28 22:18:18 +0700

    done variable

--- 5.variables/example.go
+++ 5.variables/example.go
@@ -12,4 +12,16 @@ func main(){
 	y := 42
 	fmt.Println(y)
 	fmt.Println("y is of type %T\n", y)
+	// Mixed Variable Declaration in Go
+	var a, b, c = 	0, 0, "hello world"
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Printf("a is of type %T\n", a)
+	fmt.Printf("b is of type %T\n", b)
+	fmt.Printf("c is of type %T\n", c)
+	// The lvalues and the rvalues in Go
+	/*
+		Variable = value
+	*/
 }
