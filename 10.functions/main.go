386d4e32d275b2a27bbcd2cdc6a906fb226edef7
hungvx <vuxuanhungmta@gmail.com>
2022-06-29 14:37:01 +0700

    basic function

---
+++ 10.functions/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main()  {
+	/* local variable definition */
+	var a int = 100
+	var b int = 200
+	var ret int
+
+	/* calling a function to get max value */
+	ret = max(a, b)
+
+	fmt.Printf( "Max value is : %d\n", ret )
+}
+
+// Declare function
+func max(num1 int, num2 int)  int{
+	var result int
+	if(num1 > num2){
+		result = num1
+	}else{
+		result = num2
+	}
+	return result
+}
