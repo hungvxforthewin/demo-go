91016ada883313b35250e6fc3afb4438c9562f7d
hungvx <vuxuanhungmta@gmail.com>
2022-06-30 09:12:12 +0700

    array basic

---
+++ 13.arrays/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+	// Declare array
+	var myArray [5] int
+	for i := 0; i < 5; i++ {
+		myArray[i] = i + 100 /* set element at location i to i + 100 */
+	}
+	for i := 0; i < len(myArray); i++ {
+		fmt.Printf("%d ", myArray[i])
+	}
+	fmt.Printf("\n")
+	fmt.Println("\n")
+	// Initializing Arrays
+	// var balance = [5]float32{1000.0, 2.0, 3.4, 7.0, 50.0}
+	arr := []int{1, 2, 3, 4, 5}
+	for i := 0; i < len(arr); i++ {
+		fmt.Printf("%d ", arr[i])
+	}
+	// Accessing Array Elements
+	var salary int = arr[4]
+	fmt.Println(salary)
+}
