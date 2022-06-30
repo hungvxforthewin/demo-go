db19ba2fde89936224fbdd995e25b1ca61fadf03
hungvx <vuxuanhungmta@gmail.com>
2022-06-30 17:01:58 +0700

    recusors

---
+++ 19.recusors/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// ĐỆ QUI
+	var i int = 15
+	fmt.Printf("Factorial of %d is %d", i, factorial(i))
+}
+
+func factorial(i int) int {
+	if i < 1 {
+		return 1
+	}
+	return i * factorial(i-1)
+}
