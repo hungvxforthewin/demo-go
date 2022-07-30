41bed91ed2e92d516cb20c6859ec0eec8eea8ca4
hungvx <vuxuanhungmta@gmail.com>
2022-07-30 23:47:28 +0700

    dictionary map

--- yuh/vol9-map-struct/main.go
+++ yuh/vol9-map-struct/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	// kay-value dictionary
 	studentNameAgeMap := make(map[string]int)
 	studentNameAgeMap = map[string]int {
 		"A": 11,
@@ -12,6 +13,11 @@ func main() {
 	studentNameAgeMap["E"] = 21
 	delete(studentNameAgeMap, "E")
 	_, isExits := studentNameAgeMap["E"]
+	// copy pointer
+	copySty := studentNameAgeMap
+	copySty["A"] = 777
 	fmt.Println(studentNameAgeMap)
+	fmt.Println(copySty)
 	fmt.Println(isExits)
+	fmt.Println(len(studentNameAgeMap))
 }
